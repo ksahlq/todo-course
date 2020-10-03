41f5937b856d9919c906162ca40930524594e578
ksahlq <khalid.sahlqeath@outlook.com>
2020-10-03 13:05:36 +0200

    todo button now adding a list of todo's

--- app.js
+++ app.js
@@ -10,4 +10,26 @@ todoButton.addEventListener("click", addTodo);
 
 function addTodo(event) {
   event.preventDefault();
+  //Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //CHECK TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+  //Clear Todo INPUT VALUE
+  todoInput.value = "";
 }
